0e36516bfc76e8231b857a3c9fb4a296b70e53ba
Ankur Seth <iankurseth@gmail.com>
2024-07-13 18:38:31 +0530

    Just printing args

--- bino.c
+++ bino.c
@@ -2,7 +2,12 @@
 
 
 
-int main () {
-	printf("Hello, World!\n");
+int main (int argc, char const *argv[])
+{
+	for (int i = 0; i < argc; ++i)
+	{
+		printf("Arg %d: %s\n", i+1, argv[i]);
+	}
+
 	return 0;
 }
