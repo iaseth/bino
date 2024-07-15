aa9d0882ff76c961027fef364ea22fcec963a73b
Ankur Seth <iankurseth@gmail.com>
2024-07-15 19:24:43 +0530

    Delaying buffer creation till file is confirmed to exist

--- src/bino.c
+++ src/bino.c
@@ -9,24 +9,18 @@ int main (int argc, char const *argv[])
 	int number_of_times_to_read = 4;
 
 	if (argc == 1) {
-		printf("Usage: bino $filepath\n");
+		printf("Usage:\n\tbino $filepath\n");
 		return 0;
 	}
 
 	const char *filepath = argv[1];
 
-	if (argc > 2)
-	{
+	if (argc > 2) {
 		number_of_bytes_to_read = atoi(argv[2]);
-		if (argc > 3)
-		{
+		if (argc > 3) {
 			number_of_times_to_read = atoi(argv[3]);
 		}
 	}
-
-	unsigned char *buffer = NULL;
-	int buffer_size = number_of_bytes_to_read;
-	buffer = malloc(number_of_bytes_to_read);
 
 	FILE *ptr = fopen(filepath,"rb");
 	if (ptr == NULL) {
@@ -34,13 +28,15 @@ int main (int argc, char const *argv[])
 		return 0;
 	}
 
-	for (int i = 0; i < number_of_times_to_read; ++i)
-	{
+	unsigned char *buffer = NULL;
+	int buffer_size = number_of_bytes_to_read;
+	buffer = malloc(number_of_bytes_to_read);
+
+	for (int i = 0; i < number_of_times_to_read; ++i) {
 		printf("Line %3d [%d]:", i+1, number_of_bytes_to_read);
 
 		fread(buffer, buffer_size, 1, ptr);
-		for(int j = 0; j < buffer_size; j++)
-		{
+		for (int j = 0; j < buffer_size; j++) {
 			if (j > 0 && j % 4 == 0) {
 				printf(" . ");
 			} else {
